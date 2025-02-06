c94989a0be00ff34e6524984d0d941bd5372ccf5
SS56Bo <sohamsarkar345@gmail.com>
2025-02-07 01:20:51 +0530

    ETH Wallet: Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,58 @@
 import { useState } from "react";
 import { ethers } from "ethers";
+import { Wallet, HDNodeWallet } from "ethers";
+import { Keypair } from "@solana/web3.js";
+import nacl from "tweetnacl";
 
 function App() {
   const [mnemonic, setMnemonic] = useState("");
-  const [address, setAddress] = useState("");
-  const [privKey, setPrivateKey] = useState("");
-  //function to create a seed phrase
+
   function createSeedPhrase() {
-    const walletC = ethers.Wallet.createRandom();
-    const seedPhrase = walletC.mnemonic.phrase;
+    const wallet = ethers.Wallet.createRandom();
+    const seedPhrase = wallet.mnemonic.phrase;
     setMnemonic(seedPhrase);
-    setAddress(walletC.address);
-    setPrivateKey(walletC.privateKey);
   }
 
   return (
     <>
       <div>
-        <h2>Hello, this an working HD Wallet Creator!</h2>
+        <h2>Hello, this is a working HD Wallet Creator!</h2>
       </div>
       <div>
         <h3>
           Mnemonic Phrase: <input type="text" value={mnemonic} readOnly />
         </h3>
-        <h3>
-          Address: <input type="text" value={address} readOnly />
-        </h3>
-        <h3>
-          Private Key: <input type="text" value={privKey} readOnly />
-        </h3>
-        <button onClick={createSeedPhrase}>Generate Seed Phrase </button>
+        <button onClick={createSeedPhrase}>Generate Seed Phrase</button>
       </div>
+      <EthWallet mnemonic={mnemonic} />
     </>
   );
 }
+
+export const EthWallet = ({ mnemonic }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [addresses, setAddresses] = useState([]);
+
+  return (
+    <div>
+      <button
+        onClick={async function () {
+          if (!mnemonic) return;
+          const hdNode = HDNodeWallet.fromPhrase(mnemonic);
+          const child = hdNode.deriveChild(currentIndex);
+          const wallet = new Wallet(child.privateKey);
+          setCurrentIndex(currentIndex + 1);
+          setAddresses([...addresses, wallet.address]);
+        }}
+      >
+        Add ETH wallet
+      </button>
+
+      {addresses.map((p, index) => (
+        <div key={index}>Eth - {p}</div>
+      ))}
+    </div>
+  );
+};
 
 export default App;
--- src/Greetings.jsx
+++
@@ -1,5 +0,0 @@
-function Greetings() {
-  return <h2>Heelo</h2>;
-}
-
-export default Greetings;
