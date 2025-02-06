1419279a22ad37f50dec38ad4b02bc6a773473e6
SS56Bo <sohamsarkar345@gmail.com>
2025-02-06 18:19:30 +0530

    Some Ethers Wallet

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,31 @@ import { ethers } from "ethers";
 
 function App() {
   const [mnemonic, setMnemonic] = useState("");
-
+  const [address, setAddress] = useState("");
+  const [privKey, setPrivateKey] = useState("");
   //function to create a seed phrase
   function createSeedPhrase() {
-    const seedPhrase = ethers.Wallet.createRandom().mnemonic.phrase;
+    const walletC = ethers.Wallet.createRandom();
+    const seedPhrase = walletC.mnemonic.phrase;
     setMnemonic(seedPhrase);
+    setAddress(walletC.address);
+    setPrivateKey(walletC.privateKey);
   }
 
   return (
     <>
       <div>
-        <h2>Hello</h2>
+        <h2>Hello, this an working HD Wallet Creator!</h2>
       </div>
       <div>
         <h3>
-          Seed Phrase: <input type="text" value={mnemonic} readOnly />
+          Mnemonic Phrase: <input type="text" value={mnemonic} readOnly />
+        </h3>
+        <h3>
+          Address: <input type="text" value={address} readOnly />
+        </h3>
+        <h3>
+          Private Key: <input type="text" value={privKey} readOnly />
         </h3>
         <button onClick={createSeedPhrase}>Generate Seed Phrase </button>
       </div>
