a334e6619f27632958c8eec162a4b780fddf2e7a
SS56Bo <sohamsarkar345@gmail.com>
2025-02-07 02:07:14 +0530

    Wallet Done!

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
-import { ethers } from "ethers";
+import { ethers, getBytes } from "ethers";
 import { Wallet, HDNodeWallet } from "ethers";
 import { Keypair } from "@solana/web3.js";
-import nacl from "tweetnacl";
+import "./App.css";
 
 function App() {
   const [mnemonic, setMnemonic] = useState("");
@@ -25,6 +25,7 @@ function App() {
         <button onClick={createSeedPhrase}>Generate Seed Phrase</button>
       </div>
       <EthWallet mnemonic={mnemonic} />
+      <SOLWallet mnemonic={mnemonic} />
     </>
   );
 }
@@ -50,6 +51,34 @@ export const EthWallet = ({ mnemonic }) => {
 
       {addresses.map((p, index) => (
         <div key={index}>Eth - {p}</div>
+      ))}
+    </div>
+  );
+};
+
+export const SOLWallet = ({ mnemonic }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [publicKeys, setPublicKeys] = useState([]);
+
+  return (
+    <div>
+      <button
+        onClick={async function () {
+          if (!mnemonic) return;
+          const hdNode = HDNodeWallet.fromPhrase(mnemonic, "ed25519");
+          const child = hdNode.deriveChild(currentIndex);
+          const privateKeyBytes = getBytes(child.privateKey);
+          const seed = privateKeyBytes.slice(0, 32);
+          const keypair = Keypair.fromSeed(Uint8Array.from(seed));
+          setCurrentIndex(currentIndex + 1);
+          setPublicKeys([...publicKeys, keypair.publicKey.toBase58()]);
+        }}
+      >
+        Add Solana Wallet
+      </button>
+
+      {publicKeys.map((p, index) => (
+        <div key={index}>Solana - {p}</div>
       ))}
     </div>
   );
